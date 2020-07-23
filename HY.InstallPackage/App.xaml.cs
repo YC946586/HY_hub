0f4479d5570ce43648daba63d9780e4a8fb06ae6
2391861759 <2391861759@qq.com>
2020-07-23 09:01:36 +0800

    安装卸载和更新

---
+++ HY.InstallPackage/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace HY.InstallPackage
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool createdNew;
+            string mutexName = "InstallPackage";
+            Mutex singleInstanceWatcher = new Mutex(false, mutexName, out createdNew);
+            if (!createdNew)
+            {
+                Environment.Exit(-1);
+            }
+            System.Diagnostics.Process[] p = System.Diagnostics.Process.GetProcessesByName("HY Main");
+            if (p != null && p.Count() > 0)
+            {
+                MessageBox.Show("请退出正在运行的“黑鹰 Hub”程序");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
